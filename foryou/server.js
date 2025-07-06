748ff90957d12b9f578d43eee8ddda090f5f65c9
Luke <eloprepotchy@gmail.com>
2025-07-06 18:10:41 +0800

    Update server.js

--- foryou/server.js
+++ foryou/server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 // Optional CORS settings if you’re serving frontend separately (e.g., from 127.0.0.1:5500)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://elopre0701.github.io');
+  res.header('Access-Control-Allow-Origin', 'https://aris070103.github.io');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   next();
